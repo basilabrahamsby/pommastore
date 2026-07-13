3f3e81e57de5c9b4c5e10f0bc5f7cc0b02fa1360
teqmatessolutions-gif <teqmatessolutions@gmail.com>
2026-07-13 13:48:06 +0530

    fix(mobile): update homepage provider route prefix from /homepage to /storefront/homepage

--- mobile/lib/features/catalog/homepage_provider.dart
+++ mobile/lib/features/catalog/homepage_provider.dart
@@ -5,6 +5,6 @@ final apiClientProvider = Provider<ApiClient>((ref) => ApiClient());
 
 final homepageDataProvider = FutureProvider<Map<String, dynamic>>((ref) async {
   final client = ref.read(apiClientProvider);
-  final res = await client.dio.get('/homepage');
+  final res = await client.dio.get('/storefront/homepage');
   return res.data as Map<String, dynamic>;
 });
