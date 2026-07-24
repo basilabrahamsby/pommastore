cb0a88f8fc2109913ddbe495c67450ad0ae0636d
teqmatessolutions-gif <teqmatessolutions@gmail.com>
2026-07-24 21:40:54 +0530

    Remove scratch_test_posh_sender.py

--- backend/app/scratch_test_posh_sender.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app.services.email import send_otp_email
-
-print("Sending instant OTP from sales@poshgallery.ae to basilabrahamsby@gmail.com...")
-res = send_otp_email("basilabrahamsby@gmail.com", "441188")
-print("RESULT:", res)
