7c2a1baac5d742b3f341e614768b3b9aa3cfe262
teqmatessolutions-gif <teqmatessolutions@gmail.com>
2026-07-24 22:24:32 +0530

    Remove scratch_test_final_gmail.py

--- backend/app/scratch_test_final_gmail.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app.services.email import send_otp_email
-
-print("Sending 0-second OTP to basilabrahamsby@gmail.com...")
-res = send_otp_email("basilabrahamsby@gmail.com", "882299")
-print("RESULT:", res)
