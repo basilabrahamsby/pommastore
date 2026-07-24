97543fd0b1da71dec68f3f27e08bd4078b5ea316
teqmatessolutions-gif <teqmatessolutions@gmail.com>
2026-07-24 21:14:13 +0530

    Remove scratch_test_otp.py

--- backend/app/scratch_test_otp.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app.services.email import send_otp_email
-
-print("Testing send_otp_email...")
-res = send_otp_email("sales@poshgallery.ae", "889900")
-print("OTP EMAIL TRANSMISSION RESULT:", res)
