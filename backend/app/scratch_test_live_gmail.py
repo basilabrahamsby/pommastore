6227b8ae7cc5a85461e6f38f637d2f29db11a90a
teqmatessolutions-gif <teqmatessolutions@gmail.com>
2026-07-24 22:02:57 +0530

    Remove scratch_test_live_gmail.py

--- backend/app/scratch_test_live_gmail.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app.services.email import send_otp_email
-
-print("Sending instant OTP to basilabrahamsby@gmail.com via Gmail SMTP...")
-res = send_otp_email("basilabrahamsby@gmail.com", "774411")
-print("GMAIL DISPATCH RESULT:", res)
